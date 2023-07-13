3d6c095df46d5ed59a726632d6a279bf54523354
alec-alec <twkhuynh@gmail.com>
2023-07-12 20:59:49 -0400

    sql db set up

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
+import express from "express"
+import mysql from "mysql"
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const db = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"test"
+})
+
+app.get("/", (req,res)=>{
+  res.json("hello this is the backend")
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
